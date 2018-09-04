f4ac3d30c4e9c08945fe493598cbe27c8680ec92
Salvador Becerra <Salbecerra1023@gmail.com>
2018-09-04 11:26:04 -0700

    added mongoDB and review model

--- rotten-potatoes/app.js
+++ rotten-potatoes/app.js
@@ -1,18 +1,33 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
+/*
 let reviews = [
   { title: "Great Review" },
   { title: "Next Review" }
-]
+]*/
+
+
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
 
 app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+const Review = mongoose.model('Review', {
+  title: String
+});
